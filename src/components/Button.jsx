ee86c0f78f6a07ac6a36729034ba13cd1bbc9c4a
hreinn <hreinngy@gmail.com>
2023-03-27 10:20:03 +0200

    Adds list of all the products on Home component. Adds error and loader elements to layout CSS

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,19 @@
-function Button({ color, children }) {
+function Button({ bgColor, color, border, children, onClick }) {
   const styles = {
     padding: '0.5rem 0',
-    color: 'rgba(255, 255, 255, 0.87)',
-    backgroundColor: color ? color : '#3182ce',
+    color: color ? color : 'rgba(255, 255, 255, 0.87)',
+    backgroundColor: bgColor ? bgColor : '#3182ce',
     width: '100%',
     borderRadius: '4px',
+    fontWeight: '600',
+    border: border ? '2px solid' : null,
   };
 
-  return <button style={styles}>{children}</button>;
+  return (
+    <button onClick={onClick} style={styles}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,34 @@
-import { Link } from 'react-router-dom';
+import { useApi } from '../../hooks/useApi.js';
+import { API_BASE_URL } from '../../settings/api.js';
+import ProductItem from '../ProductItem.jsx';
+import styles from './Home.module.scss';
 
 function Home() {
+  const { data, isLoading, isError } = useApi(API_BASE_URL);
+
+  if (isError) {
+    return <div className={'error'}>Something went wrong.. please try again later</div>;
+  }
+
   return (
     <section>
       <div className={'container'}>
         <h1>Home</h1>
-        <Link to={'/cart'}>Placeholder link</Link>
+        {isLoading && (
+          <div className={'loader-container'}>
+            <div className={'loader'}></div>
+          </div>
+        )}
+        <div className={styles.home}>
+          <div className={'products-container'}>
+            <h2>All Products</h2>
+            <div className={'products'}>
+              {data.map((product) => {
+                return <ProductItem {...product} />;
+              })}
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
