cdf4fe4e20b9d0d8a18cd111650e585d1f4bc68f
hreinn <hreinngy@gmail.com>
2023-03-28 10:45:15 +0200

    Adds responsive Product details component

--- src/components/pages/ProductDetails.jsx
+++ src/components/pages/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useApi } from '../../hooks/useApi.js';
 import { API_BASE_URL } from '../../settings/api.js';
 import styles from './ProductDetails.module.scss';
@@ -9,6 +9,7 @@ function ProductDetails() {
   const { data, isLoading, isError } = useApi(API_BASE_URL + '/' + id);
   const { title, description, imageUrl, discountedPrice, price, rating, reviews } = data;
   const discount = price - discountedPrice;
+  const navigate = useNavigate();
 
   if (isError) {
     return <div className={'error'}>Something went wrong.. please try again later</div>;
@@ -17,18 +18,24 @@ function ProductDetails() {
   return (
     <section>
       <div className={'container'}>
-        <h1>Product details</h1>
+        <h1>{title}</h1>
         {isLoading && (
           <div className={'loader-container'}>
             <div className={'loader'}></div>
           </div>
         )}
+
         <div className={styles.productDetails}>
-          <div className={'details'}>
-            <div>
-              <h2>{title}</h2>
-              <p>{description}</p>
-              <p className={'rating'}>Rating: {rating > 0 ? <span>{rating}</span> : 'Not rated'}</p>
+          <button className={'back-btn'} onClick={() => navigate(-1)}>
+            {'<< '}Go back
+          </button>
+          <div className={'details-container'}>
+            <div className={'product-image'}>
+              <img src={imageUrl} alt={title} />
+            </div>
+            <div className={'details'}>
+              <h2>{description}</h2>
+              <p className={'rating'}>Rating: {rating > 0 ? <span>{rating} stars</span> : 'Not rated'}</p>
               <div className={'price'}>
                 <p>
                   Price:
@@ -47,22 +54,26 @@ function ProductDetails() {
               </div>
               <Button>Add to Cart</Button>
             </div>
-            <div>
-              <img src={imageUrl} alt={title} />
+          </div>
+          <div className={'reviews'}>
+            <h3>Reviews</h3>
+            <div className={'reviews-container'}>
+              {reviews && reviews.length ? (
+                reviews.map(({ username, rating, description }, index) => {
+                  return (
+                    <div key={index}>
+                      <b>
+                        {username} <span>rated this {rating} stars</span>
+                      </b>
+                      <p>{description}</p>
+                    </div>
+                  );
+                })
+              ) : (
+                <div>No reviews yet</div>
+              )}
             </div>
           </div>
-          {reviews
-            ? reviews.map(({ username, rating, description }, index) => {
-                return (
-                  <div key={index}>
-                    <p>
-                      {username} <span>Rating: {rating}</span>
-                    </p>
-                    <p>{description}</p>
-                  </div>
-                );
-              })
-            : null}
         </div>
       </div>
     </section>
